39895863f25f98e44a3d343c8893020bde1c4f39
Zach Lambert <zachalambert@gmail.com>
2025-05-10 09:27:48 +0100

    Reverting padding changes

--- include/datagui/types/prop_stack.hpp
+++ include/datagui/types/prop_stack.hpp
@@ -3,14 +3,11 @@
 #include <assert.h>
 #include <cstdint>
 #include <memory>
-#include <span>
 #include <typeindex>
 #include <unordered_map>
 #include <vector>
 
 namespace datagui {
-
-static constexpr std::size_t alignment = 8;
 
 class PropHandler {
   class TypeBase {
@@ -25,19 +22,14 @@ class PropHandler {
   class TypeImpl : public TypeBase {
   public:
     void copy(const std::uint8_t* from, std::uint8_t* to) override {
-      assert(long(from) % alignment == 0);
-      assert(long(to) % alignment == 0);
       new ((T*)to) T(*(const T*)from);
     }
 
     void move(std::uint8_t* from, std::uint8_t* to) override {
-      assert(long(from) % alignment == 0);
-      assert(long(to) % alignment == 0);
       new ((T*)to) T(std::move(*(const T*)from));
     }
 
     void destruct(std::uint8_t* ptr) const override {
-      assert(long(ptr) % alignment == 0);
       ((T*)ptr)->~T();
     }
 
@@ -89,9 +81,7 @@ template <typename K>
 struct PropHeader {
   std::type_index type;
   K key;
-  int padding;
-  PropHeader(const K& key, std::type_index type) :
-      key(key), type(type), padding(0) {}
+  PropHeader(const K& key, std::type_index type) : key(key), type(type) {}
 };
 
 template <typename K>
@@ -108,22 +98,22 @@ public:
     if (header().type != std::type_index(typeid(T))) {
       return nullptr;
     }
-    return (const T*)(data.data() + offset + sizeof(Header));
+    return (const T*)(data->data() + offset + sizeof(Header));
   }
 
 private:
   PropValue(
-      std::span<const std::uint8_t> data,
+      const std::vector<std::uint8_t>* data,
       std::size_t offset,
       const PropHandler* handler) :
       data(data), offset(offset), handler(handler) {}
 
-  std::span<const std::uint8_t> data;
+  const std::vector<std::uint8_t>* data;
   std::size_t offset;
   const PropHandler* handler;
 
   const Header& header() const {
-    return *((Header*)(data.data() + offset));
+    return *((Header*)(data->data() + offset));
   }
 
   template <typename K_>
@@ -143,7 +133,7 @@ public:
 
   // Prefix
   PropIterator& operator++() {
-    const auto& header = *((Header*)(data.data() + offset));
+    const auto& header = *((Header*)(data->data() + offset));
     offset += sizeof(Header);
     offset += handler->size(header.type);
     return *this;
@@ -157,7 +147,7 @@ public:
   }
 
   friend bool operator==(const PropIterator& lhs, const PropIterator& rhs) {
-    return lhs.data.data() == rhs.data.data() && lhs.offset == rhs.offset;
+    return lhs.data == rhs.data && lhs.offset == rhs.offset;
   }
   friend bool operator!=(const PropIterator& lhs, const PropIterator& rhs) {
     return !(lhs == rhs);
@@ -165,12 +155,12 @@ public:
 
 private:
   PropIterator(
-      std::span<const std::uint8_t> data,
+      const std::vector<std::uint8_t>* data,
       std::size_t offset,
       const PropHandler* handler) :
       data(data), offset(offset), handler(handler) {}
 
-  std::span<const std::uint8_t> data;
+  const std::vector<std::uint8_t>* data;
   std::size_t offset;
   const PropHandler* handler;
 
@@ -183,73 +173,42 @@ class PropContainer {
   using Header = PropHeader<K>;
 
 public:
-  PropContainer() :
-      alloc_ptr(nullptr), alloc_size(0), data(nullptr), data_size(0) {}
+  PropContainer() = default;
 
   ~PropContainer() {
     clear_from(0);
-    if (alloc_ptr) {
-      free(alloc_ptr);
-    }
   }
 
-  void resize(std::size_t new_data_size) {
-    // Allocate via std::uint64_t to ensure a 64-bit byte boundary
-
-    if (new_data_size < data_size) {
-      // TODO: Shrink the allocated memory?
-      data_size = new_data_size;
-      return;
-    }
-
-    std::size_t required_alloc_size = new_data_size / 8;
-    if (new_data_size % 8 != 0) {
-      required_alloc_size++;
-    }
-
-    std::size_t new_alloc_size = alloc_size == 0 ? 1 : 2 * alloc_size;
-    while (new_alloc_size < required_alloc_size) {
-      new_alloc_size *= 2;
-    }
-    printf("Alloc from: %p %zu\n", alloc_ptr, alloc_size);
-    alloc_ptr = (std::uint64_t*)realloc((void*)alloc_ptr, new_alloc_size * 8);
-    printf("Alloc to: %p %zu\n", alloc_ptr, new_alloc_size);
-
-    data = (std::uint8_t*)alloc_ptr;
-    data_size = new_data_size; // May be below new_alloc_size/8
-  }
-
-  PropContainer(const PropContainer& other) : handler(other.handler) {
+  PropContainer(const PropContainer& other) : data(), handler(other.handler) {
     std::size_t offset = 0;
-    resize(other.size());
-    while (offset < other.size()) {
-      const auto& header = *((const Header*)(other.data + offset));
+    data.resize(other.data.size());
+    while (offset < other.data.size()) {
+      const auto& header = *((const Header*)(other.data.data() + offset));
       offset += sizeof(Header);
-      handler.copy(header.type, other.data + offset, data + offset);
+      handler.copy(
+          header.type,
+          other.data.data() + offset,
+          data.data() + offset);
       offset += handler.size(header.type);
     }
   }
 
-  PropContainer(PropContainer&& other) :
-      alloc_ptr(other.alloc_ptr),
-      alloc_size(other.alloc_size),
-      data(other.data),
-      data_size(other.data_size),
-      handler(std::move(other.handler)) {
-    other.alloc_ptr = nullptr;
-  }
+  PropContainer(PropContainer&& other) = default;
 
   PropContainer& operator=(const PropContainer& other) {
     clear_from(0);
     handler = other.handler;
 
     std::size_t offset = 0;
-    resize(other.size());
+    data.resize(other.data.size());
 
-    while (offset < other.size()) {
-      const auto& header = *((const Header*)(other.data + offset));
+    while (offset < other.data.size()) {
+      const auto& header = *((const Header*)(other.data.data() + offset));
       offset += sizeof(Header);
-      handler.copy(header.type, other.data + offset, data + offset);
+      handler.copy(
+          header.type,
+          other.data.data() + offset,
+          data.data() + offset);
       offset += handler.size(header.type);
     }
 
@@ -259,41 +218,37 @@ public:
   PropContainer& operator==(PropContainer&& other) {
     clear_from(0);
     handler = std::move(other.handler);
-    alloc_ptr = other.alloc_ptr;
-    other.alloc_ptr = nullptr;
-    alloc_size = other.alloc_size;
-    data = other.data;
-    data_size = other.data_size;
+    data = std::move(other.data);
   }
 
   void clear_from(std::size_t start_offset) {
     std::size_t offset = start_offset;
-    while (offset < data_size) {
-      auto& header = *((Header*)(data + offset));
+    while (offset < data.size()) {
+      auto& header = *((Header*)(data.data() + offset));
       std::type_index type = header.type;
 
       header.~Header();
       offset += sizeof(header);
 
-      handler.destruct(type, data + offset);
+      handler.destruct(type, data.data() + offset);
       offset += handler.size(type);
     }
-    assert(offset <= data_size);
-    data_size = start_offset;
+    assert(offset <= data.size());
+    data.resize(start_offset);
   }
 
   template <typename T>
   std::size_t push(const K& key, T&& value) {
-    std::size_t start_offset = data_size;
+    std::size_t start_offset = data.size();
 
     Header header(key, std::type_index(typeid(T)));
 
-    resize(data_size + sizeof(Header));
-    new ((Header*)(data + data_size - sizeof(Header)))
+    data.resize(data.size() + sizeof(Header));
+    new ((Header*)(data.data() + data.size() - sizeof(Header)))
         Header(std::move(header));
 
-    resize(data_size + sizeof(T));
-    new ((T*)(data + data_size - sizeof(T))) T(std::forward<T>(value));
+    data.resize(data.size() + sizeof(T));
+    new ((T*)(data.data() + data.size() - sizeof(T))) T(std::forward<T>(value));
 
     handler.require<T>();
 
@@ -301,7 +256,7 @@ public:
   }
 
   std::size_t push(const PropValue<K>& value) {
-    std::size_t start_offset = data_size;
+    std::size_t start_offset = data.size();
 
     const auto& value_header =
         *((const Header*)(value.data->data() + value.offset));
@@ -309,61 +264,53 @@ public:
 
     std::size_t type_size = handler.size(value_header.type);
 
-    resize(data_size + sizeof(Header));
-    new ((Header*)(data + data_size - sizeof(Header))) Header(value_header);
+    data.resize(data.size() + sizeof(Header));
+    new ((Header*)(data.data() + data.size() - sizeof(Header)))
+        Header(value_header);
 
-    resize(data_size + type_size);
+    data.resize(data.size() + type_size);
     handler.copy(
         value_header.type,
         value.data->data() + value.offset + sizeof(Header),
-        data + data_size - type_size);
+        data.data() + data.size() - type_size);
 
     return start_offset;
   }
 
   template <typename T>
   void replace(std::size_t offset, const K& key, T&& value) {
-    assert(offset + sizeof(Header) + sizeof(T) <= data_size);
-    const auto& header = *((Header*)(data + offset));
+    assert(offset + sizeof(Header) + sizeof(T) <= data.size());
+    const auto& header = *((Header*)(data.data() + offset));
     assert(header.key == key);
     assert(header.type == std::type_index(typeid(T)));
-    *((std::decay_t<T>*)(data + offset + sizeof(Header))) =
+    *((std::decay_t<T>*)(data.data() + offset + sizeof(Header))) =
         std::forward<T>(value);
   }
 
   template <typename T>
   const T* get(std::size_t offset, const K& key) const {
-    const auto& header = *((const Header*)(data + offset));
+    const auto& header = *((const Header*)(data.data() + offset));
     if (header.key != key) {
       return nullptr;
     }
     assert(header.type == std::type_index(typeid(T)));
-    return (const T*)(data + offset + sizeof(Header));
+    return (const T*)(data.data() + offset + sizeof(Header));
   }
 
   std::size_t size() const {
-    return data_size;
+    return data.size();
   }
 
   PropIterator<K> begin() const {
-    return PropIterator<K>(
-        std::span<std::uint8_t>(data, data_size),
-        0,
-        &handler);
+    return PropIterator<K>(&data, 0, &handler);
   }
 
   PropIterator<K> end() const {
-    return PropIterator<K>(
-        std::span<std::uint8_t>(data, data_size),
-        data_size,
-        &handler);
+    return PropIterator<K>(&data, data.size(), &handler);
   }
 
 private:
-  std::uint64_t* alloc_ptr;
-  std::size_t alloc_size;
-  std::uint8_t* data;
-  std::size_t data_size;
+  std::vector<std::uint8_t> data;
   PropHandler handler;
 };
 
