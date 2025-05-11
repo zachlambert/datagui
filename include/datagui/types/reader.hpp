3dd9b6dba6b7803118d5fd4388a1ff7052cdaeae
Zach Lambert <zachalambert@gmail.com>
2025-05-11 22:06:36 +0100

    Created reader stub

---
+++ include/datagui/types/reader.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "datagui/gui.hpp"
+#include <datapack/reader.hpp>
+
+namespace datagui {
+
+class GuiReader : public datapack::Reader {
+public:
+  GuiReader(Gui& gui) : gui(gui) {}
+
+  void integer(datapack::IntType type, void* value) override;
+  void floating(datapack::FloatType type, void* value) override;
+  bool boolean() override;
+  const char* string() override;
+  int enumerate(const std::span<const char*>& labels) override;
+  std::tuple<const std::uint8_t*, std::size_t> binary(
+      std::size_t length,
+      std::size_t stride) override;
+
+  bool optional_begin() override;
+  void optional_end() override;
+
+  int variant_begin(const std::span<const char*>& labels) override;
+  void variant_end() override;
+
+  void object_begin(std::size_t) override;
+  void object_end(std::size_t) override;
+  void object_next(const char* key) override;
+
+  void tuple_begin(std::size_t trivial_size = 0) override;
+  void tuple_end(std::size_t trivial_size = 0) override;
+  void tuple_next() override;
+
+  void list_begin(bool is_trivial = false) override;
+  bool list_next() override;
+  void list_end() override;
+
+private:
+  Gui& gui;
+};
+
+} // namespace datagui
