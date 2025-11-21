4f93fd97227faf9339587f4acad2fc409091a0d1
Zach Lambert <zachalambert@gmail.com>
2025-11-21 22:26:29 +0000

    Updated checkbox

--- src/system/checkbox.cpp
+++ src/system/checkbox.cpp
@@ -1,62 +1,62 @@
-#include "datagui/element/checkbox.hpp"
+#include "datagui/system/checkbox.hpp"
 
 namespace datagui {
 
-void CheckboxSystem::set_input_state(
-    Element& e,
-    const ConstElementList& children) {
-  const auto& props = *e.props.cast<CheckboxProps>();
+void CheckboxSystem::set_input_state(ElementPtr element) {
+  auto& state = element.state();
 
   float size = fm->text_height(theme->text_font, theme->text_size);
-  e.fixed_size = Vecf::Constant(size);
-  e.dynamic_size = Vecf::Zero();
-  e.floating = false;
+  state.fixed_size = Vecf::Constant(size);
+  state.dynamic_size = Vecf::Zero();
+  state.floating = false;
 }
 
-void CheckboxSystem::render(const Element& e, Renderer& renderer) {
-  const auto& props = *e.props.cast<CheckboxProps>();
+void CheckboxSystem::render(ConstElementPtr element, Renderer& renderer) {
+  const auto& state = element.state();
+  const auto& checkbox = element.checkbox();
 
   renderer.queue_box(
-      e.box(),
+      state.box(),
       theme->input_color_bg,
       theme->input_border_width,
       theme->input_color_border,
       0);
 
-  if (!props.checked) {
+  if (!checkbox.checked) {
     return;
   }
 
   Boxf icon_box;
   icon_box.lower = minimum(
-      e.position + Vecf::Constant(theme->input_border_width * 2.f),
-      e.box().center());
+      state.position + Vecf::Constant(theme->input_border_width * 2.f),
+      state.box().center());
   icon_box.upper = maximum(
-      e.position + e.size - Vecf::Constant(theme->input_border_width * 2.f),
-      e.box().center());
+      state.position + state.size -
+          Vecf::Constant(theme->input_border_width * 2.f),
+      state.box().center());
 
   renderer
       .queue_box(icon_box, theme->input_color_bg_active, 0, Color::Black(), 0);
 }
 
-bool CheckboxSystem::mouse_event(Element& e, const MouseEvent& event) {
-  auto& props = *e.props.cast<CheckboxProps>();
+bool CheckboxSystem::mouse_event(ElementPtr element, const MouseEvent& event) {
+  auto& checkbox = element.checkbox();
 
   if (event.action == MouseAction::Release &&
       event.button == MouseButton::Left) {
-    props.changed = true;
-    props.checked = !props.checked;
+    checkbox.changed = true;
+    checkbox.checked = !checkbox.checked;
     return true;
   }
   return false;
 }
 
-bool CheckboxSystem::key_event(Element& e, const KeyEvent& event) {
-  auto& props = *e.props.cast<CheckboxProps>();
+bool CheckboxSystem::key_event(ElementPtr element, const KeyEvent& event) {
+  auto& checkbox = element.checkbox();
 
   if (event.action == KeyAction::Release && event.key == Key::Enter) {
-    props.changed = true;
-    props.checked = !props.checked;
+    checkbox.changed = true;
+    checkbox.checked = !checkbox.checked;
     return true;
   }
   return false;
