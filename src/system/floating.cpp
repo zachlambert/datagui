ab7afd679e6b5ddd2ced2f6e1630dca6bdecd55a
Zach Lambert <zachalambert@gmail.com>
2025-11-21 22:37:05 +0000

    Updated floating system

--- src/system/floating.cpp
+++ src/system/floating.cpp
@@ -1,38 +1,37 @@
-#include "datagui/element/floating.hpp"
+#include "datagui/system/floating.hpp"
 
 namespace datagui {
 
-void FloatingSystem::set_input_state(
-    Element& e,
-    const ConstElementList& children) {
-  auto& props = *e.props.cast<FloatingProps>();
-  e.fixed_size = Vecf::Zero();
-  e.dynamic_size = Vecf::Zero();
-  e.floating = true;
-  e.floating_type = FloatingTypeAbsolute(Vecf(props.width, props.height));
+void FloatingSystem::set_input_state(ElementPtr element) {
+  auto& state = element.state();
+  const auto& floating = element.floating();
+
+  state.fixed_size = Vecf::Zero();
+  state.dynamic_size = Vecf::Zero();
+  state.floating = true;
+  state.floating_type =
+      FloatingTypeAbsolute(Vecf(floating.width, floating.height));
 }
 
-void FloatingSystem::set_dependent_state(
-    Element& e,
-    const ElementList& children) {
-
-  auto& props = *e.props.cast<FloatingProps>();
+void FloatingSystem::set_dependent_state(ElementPtr element) {
+  auto& state = element.state();
+  auto& floating = element.floating();
 
   {
-    auto& box = props.title_bar_box;
-    box.lower = e.float_box.lower;
-    box.upper.x = e.float_box.upper.x;
-    box.upper.y = e.float_box.lower.y +
+    auto& box = floating.title_bar_box;
+    box.lower = state.float_box.lower;
+    box.upper.x = state.float_box.upper.x;
+    box.upper.y = state.float_box.lower.y +
                   fm->text_height(theme->text_font, theme->text_size) +
                   2.f * (theme->input_border_width + theme->text_padding);
   }
 
-  props.title_bar_text_width =
-      e.float_box.size().x -
+  floating.title_bar_text_width =
+      state.float_box.size().x -
       2.f * (theme->input_border_width + theme->text_padding);
 
   {
-    auto& box = props.close_button_box;
+    auto& box = floating.close_button_box;
     Vecf text_size = fm->text_size(
         "close",
         theme->text_font,
@@ -40,67 +39,69 @@ void FloatingSystem::set_dependent_state(
         LengthWrap());
 
     Vecf button_size = text_size + 2.f * Vecf::Constant(theme->text_padding);
-    props.title_bar_text_width -= (button_size.x + theme->text_padding);
-    props.title_bar_text_width = std::max(0.f, props.title_bar_text_width);
+    floating.title_bar_text_width -= (button_size.x + theme->text_padding);
+    floating.title_bar_text_width = std::max(0.f, props.title_bar_text_width);
 
-    box.upper =
-        props.title_bar_box.upper - Vecf::Constant(theme->input_border_width);
+    box.upper = floating.title_bar_box.upper -
+                Vecf::Constant(theme->input_border_width);
     box.lower = box.upper - button_size;
   }
 
-  if (children.empty()) {
+  auto child = element.child();
+  if (!child.exists()) {
     return;
   }
 
-  // Always hide excess children
-  for (std::size_t i = 1; i < children.size(); i++) {
-    children[i]->hidden = true;
+  child.state().position = floating.title_bar_box.top_left();
+  child.state().size = state.float_box.upper - child.state().position;
+
+  // Hide excess children
+  child = child.next();
+  while (child.exists()) {
+    child.state().hidden = true;
+    child = child.next();
   }
-
-  auto& child = *children[0];
-
-  child.position = props.title_bar_box.top_left();
-  child.size = e.float_box.upper - child.position;
 }
 
-void FloatingSystem::render(const Element& e, Renderer& renderer) {
-  auto& props = *e.props.cast<FloatingProps>();
+void FloatingSystem::render(ConstElementPtr element, Renderer& renderer) {
+  const auto& state = element.state();
+  const auto& floating = element.floating();
 
   const Color& bg_color =
-      props.bg_color ? *props.bg_color : theme->layout_color_bg;
+      floating.bg_color ? *floating.bg_color : theme->layout_color_bg;
 
   renderer.queue_box(
-      e.float_box,
+      state.float_box,
       bg_color,
       theme->popup_border_width,
       Color::Black(),
       0);
 
   renderer.queue_box(
-      props.title_bar_box,
+      floating.title_bar_box,
       theme->layout_color_bg,
       theme->input_border_width,
       theme->input_color_border,
       0);
 
   renderer.queue_text(
-      e.float_box.lower +
+      state.float_box.lower +
           Vecf::Constant(theme->input_border_width + theme->text_padding),
-      props.title,
+      floating.title,
       theme->text_font,
       theme->text_size,
       theme->text_color,
-      LengthFixed(props.title_bar_text_width));
+      LengthFixed(floating.title_bar_text_width));
 
   renderer.queue_box(
-      props.close_button_box,
+      floating.close_button_box,
       theme->input_color_bg,
       0,
       Color::Black(),
       0);
 
   renderer.queue_text(
-      props.close_button_box.lower + Vecf::Constant(theme->text_padding),
+      floating.close_button_box.lower + Vecf::Constant(theme->text_padding),
       "close",
       theme->text_font,
       theme->text_size,
@@ -108,17 +109,18 @@ void FloatingSystem::render(const Element& e, Renderer& renderer) {
       LengthWrap());
 }
 
-bool FloatingSystem::mouse_event(Element& e, const MouseEvent& event) {
+bool FloatingSystem::mouse_event(ElementPtr element, const MouseEvent& event) {
+  auto& floating = element.floating();
+
   if (event.action != MouseAction::Release) {
     return false;
   }
-  auto& props = *e.props.cast<FloatingProps>();
 
-  if (!props.close_button_box.contains(event.position)) {
+  if (!floating.close_button_box.contains(event.position)) {
     return false;
   }
-  props.open = false;
-  props.open_changed = true;
+  floating.open = false;
+  floating.open_changed = true;
   return true;
 }
 
