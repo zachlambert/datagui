d2d5c16242805d91a3ff701c9843f158f5c639a9
Zach Lambert <zachalambert@gmail.com>
2025-11-22 22:17:29 +0000

    Added list to datapack code

---
+++ include/datagui/element/key_list.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vector>
+
+namespace datagui {
+
+class KeyList {
+public:
+  std::size_t append() {
+    std::size_t key = next_key++;
+    keys.push_back(key);
+    return key;
+  }
+
+  std::size_t insert(std::size_t pos) {
+    std::size_t key = next_key++;
+    keys.insert(keys.begin() + pos, key);
+    return key;
+  }
+
+  void remove(std::size_t pos) {
+    keys.erase(keys.begin() + pos);
+  }
+
+  std::size_t operator[](std::size_t i) const {
+    return keys[i];
+  }
+
+  std::size_t size() const {
+    return keys.size();
+  }
+
+  std::vector<std::size_t>::const_iterator begin() const {
+    return keys.begin();
+  }
+  std::vector<std::size_t>::const_iterator end() const {
+    return keys.end();
+  }
+
+private:
+  std::vector<std::size_t> keys;
+  std::size_t next_key = 0;
+};
+
+} // namespace datagui
--- src/datapack/edit.cpp
+++ src/datapack/edit.cpp
@@ -1,29 +1,35 @@
 #include "datagui/datapack/edit.hpp"
+#include "datagui/element/key_list.hpp"
 #include "datagui/gui.hpp"
 
 namespace datagui {
 
 bool datapack_edit(Gui& gui, const datapack::Schema& schema) {
   std::stack<datapack::Schema::Iterator> stack;
+  struct ListState {
+    Var<KeyList> keys;
+    std::size_t i;
+    bool children_open;
+  };
+  std::stack<ListState> list_stack;
+
   auto iter = schema.begin();
   bool changed = false;
 
   while (iter != schema.end()) {
-    while (!stack.empty()) {
+    while (iter != schema.end() && !stack.empty()) {
       auto parent = stack.top();
 
       if (parent.object_begin()) {
         if (iter != parent.next()) {
           gui.section_end();
         }
-
-        bool object_end = false;
+        bool have_next = false;
         while (true) {
           if (iter.object_end()) {
             gui.series_end();
             stack.pop();
             iter = iter.next();
-            object_end = true;
             break;
           }
           auto object_next = iter.object_next();
@@ -34,16 +40,62 @@ bool datapack_edit(Gui& gui, const datapack::Schema& schema) {
             iter = iter.next().skip();
             continue;
           }
+          have_next = true;
           iter = iter.next();
           break;
         }
-        if (object_end) {
+        if (!have_next) {
           continue;
-        } else {
-          break;
         }
+        break;
       }
-      // TODO: Process others
+      if (parent.tuple_begin()) {
+        if (iter.tuple_end()) {
+          gui.series_end();
+          stack.pop();
+          iter = iter.next();
+          continue;
+        }
+        if (!iter.tuple_next()) {
+          throw datapack::SchemaError("Expected TupleNext");
+        }
+        iter = iter.next();
+      }
+      if (parent.list()) {
+        assert(!list_stack.empty());
+        auto& state = list_stack.top();
+
+        if (state.children_open && state.i != 0) {
+          if (gui.button("Remove")) {
+            state.keys.mut().remove(state.i);
+          } else {
+            state.i++;
+          }
+          gui.series_end();
+        }
+
+        while (state.i < state.keys->size()) {
+          gui.args_series().horizontal();
+          if (gui.series_begin()) {
+            break;
+          }
+          state.i++;
+        }
+
+        if (state.i == state.keys->size()) {
+          if (state.children_open) {
+            gui.series_end();
+          }
+          if (gui.button("new")) {
+            state.keys.mut().append();
+          }
+          gui.series_end();
+          stack.pop();
+          list_stack.pop();
+          continue;
+        }
+        iter = parent.next();
+      }
       assert(false);
     }
     if (iter == schema.end()) {
@@ -59,6 +111,21 @@ bool datapack_edit(Gui& gui, const datapack::Schema& schema) {
       iter = iter.skip();
       continue;
     }
+    if (iter.list()) {
+      if (gui.series_begin()) {
+        stack.push(iter);
+        auto keys = gui.variable<KeyList>();
+        if (gui.series_begin()) {
+          list_stack.push({keys, 0, true});
+        } else {
+          list_stack.push({keys, keys->size(), false});
+        }
+        iter = iter.next();
+      }
+      iter = iter.next().skip();
+      continue;
+    }
+
     if (iter.number()) {
       if (gui.text_input("0")) {
         changed = true;
